268a370780276afbb9fdbb4b482b9d19c02a7cb5
xavkorkor <xavkqw@gmail.com>
2026-08-01 14:55:27 +0800

    Refresh quotation app cache after letterhead fix

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,16 @@
-const CACHE='au-quotation-v1';
+const CACHE='au-quotation-v4';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./header_1.txt','./header_2.txt','./header_3.txt','./header_4.txt'];
 self.addEventListener('install',event=>{
+  self.skipWaiting();
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
 });
 self.addEventListener('activate',event=>{
-  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));
 });
 self.addEventListener('fetch',event=>{
-  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).catch(()=>caches.match('./index.html'))));
+  event.respondWith(fetch(event.request).then(response=>{
+    const copy=response.clone();
+    caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+    return response;
+  }).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html'))));
 });
