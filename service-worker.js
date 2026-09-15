7211303b29bd7190e3ca5e2724ff6edf0bafc820
xavkorkor <xavkqw@gmail.com>
2026-09-15 21:26:25 +0800

    Refresh cache for private settlement toggle

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='au-quotation-v32';
+const CACHE='au-quotation-v33';
 
 // Normal quotation startup uses compact production bundles.
 // History files remain on-demand and are cached after first use.
