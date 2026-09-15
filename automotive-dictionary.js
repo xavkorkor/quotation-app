bee1ce9cfc47ef080c45a77f111bbf0da42437b4
xavkorkor <xavkqw@gmail.com>
2026-09-15 14:08:05 +0800

    Load compact quotation actions UI

--- automotive-dictionary.js
+++ automotive-dictionary.js
@@ -73,6 +73,7 @@
     .then(()=>load('./cloud-record-integrity.js'))
     .then(()=>load('./history-permanent-delete.js'))
     .then(()=>load('./ui-workspace-v1.js'))
+    .then(()=>load('./quotation-actions-collapse.js'))
     .then(()=>load('./ui-topbar-v2.js'))
     .then(()=>load('./remarks-rich-format.js'))
     .then(()=>load('./remarks-private-settlement.js'))
