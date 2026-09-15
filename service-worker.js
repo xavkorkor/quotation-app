5144e63722325db27b13b7998d8bde221a4caf6c
xavkorkor <xavkqw@gmail.com>
2026-09-15 15:30:59 +0800

    Refresh workflow suite cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='au-quotation-v26';
+const CACHE='au-quotation-v27';
 
 // Normal quotation startup uses compact production bundles.
 // History files remain on-demand and are cached after first use.
