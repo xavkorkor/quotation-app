3da5d9465f114ead84274b5d44f6b14c5fdd0ee6
xavkorkor <xavkqw@gmail.com>
2026-09-15 15:50:02 +0800

    Refresh cache for user-owned template library

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='au-quotation-v28';
+const CACHE='au-quotation-v29';
 
 // Normal quotation startup uses compact production bundles.
 // History files remain on-demand and are cached after first use.
