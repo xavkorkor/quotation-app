cbcc92c9fb2e1ee4717327eac20b916c3be398e1
xavkorkor <xavkqw@gmail.com>
2026-09-16 09:31:32 +0800

    Bump cache for complete History reset

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='au-quotation-v34';
+const CACHE='au-quotation-v35';
 
 // Normal quotation startup uses compact production bundles.
 // History files remain on-demand and are cached after first use.
