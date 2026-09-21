c167f4f624d62944cbfdcf609051e82e6a356cdc
xavkorkor <xavkqw@gmail.com>
2026-09-21 10:46:10 +0800

    Cache new UI module and release startup speed/UI v46

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='au-quotation-v45';
+const CACHE='au-quotation-v46';
 
 // Normal quotation startup uses compact production bundles.
 // History executes on demand; its assets remain available in the offline cache.
@@ -13,6 +13,7 @@ const ASSETS=[
   './workflow-suite.js',
   './cloud-workshop-data.js',
   './operations-suite.js',
+  './workspace-v46.js',
   './system-health.js',
   './online-storage.js',
   './history-enhancements.js',
