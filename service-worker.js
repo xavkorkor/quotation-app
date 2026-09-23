c6d0b77e57821f265725db29cd76f840941a75fe
xavkorkor <xavkqw@gmail.com>
2026-09-23 13:34:00 +0800

    Bump quotation offline cache to v50 for darker PDF print contrast

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='au-quotation-v49';
+const CACHE='au-quotation-v50';
 
 // Normal quotation startup uses compact production bundles.
 // History executes on demand; its assets remain available in the offline cache.
